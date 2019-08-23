52e60308d4229a56259d698ea8ce5800f3d6886b
sxuan11 <sxuan97@outlook.com>
2019-08-23 15:07:21 +0800

    fix jsonp

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,29 @@
 
-import jsonp from 'jsonp'
+import JsonP from 'jsonp'
+export default class Axios {
+    static jsonp(options){
+        return new Promise((resolve,reject)=>{
+            JsonP(options.url,{
+                param:'callback',
+                timeout: 10000,
+            },function(err,res){
+                if(res.status === 0) {
+                    resolve(res)
+                } else {
+                    reject(err)
+                }
+            })
+        })
+    }
+}
 
-
-export function get_ip() {
-    jsonp('http://pv.sohu.com/cityjson?ie=utf-8', null, (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(data)
-        return data
-        
-      }
-    })
-  } 
+// export function get_ip() {
+//     JsonP('http://apis.map.qq.com/ws/location/v1/ip?ip%27%27&key=GWHBZ-IWZHI-OXNG4-5EVS3-6MJU6-YYB7E', {timeout:10000}, (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//           console.log(data)
+//         return data
+//       }
+//     })
+//   }
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -7,7 +7,8 @@ import {
 } from 'react-router-dom';
 
 import Util from './../util/index'
-import {get_ip} from './../api/api'
+import Axios from './../api/api.js'
+// import {get_ip} from './../api/api'
 
 
 import './index.css'
@@ -17,12 +18,14 @@ export class index extends Component {
     constructor(props){
         super(props)
         this.state={
-            location:'广东省广州市'
+            province:'',
+            city:'',
+            district:'',
         }
       }
 
 
-      componentWillMount(){
+      componentDidMount(){
           this.setState({
               userName:''
           })
@@ -32,7 +35,25 @@ export class index extends Component {
                   sysTime
               })
           },1000);
-          get_ip()
+          this.get_ip()
+      }
+
+      
+
+      get_ip(){
+        Axios.jsonp({
+            url:'http://apis.map.qq.com/ws/location/v1/ip?ip%27%27&output=jsonp&key=GWHBZ-IWZHI-OXNG4-5EVS3-6MJU6-YYB7E'
+        }).then((res)=>{
+            if(res.status===0){
+                console.log(res)
+                let data = res;
+                this.setState({
+                    province:data.result.ad_info.province,
+                    city:data.result.ad_info.city,
+                    district:data.result.ad_info.district,
+                })
+            }
+        }) 
       }
     
     render() {
@@ -74,7 +95,7 @@ export class index extends Component {
                                 position:'absolute',
                                 right:'5%'
                             }}><sapn>当前时间:{this.state.sysTime}</sapn>
-                            <p>当前城市:{this.state.location}</p></div>
+                            <p>当前城市:{this.state.province}{this.state.city}{this.state.district}</p></div>
                         </div>
                     </Header>
                     
